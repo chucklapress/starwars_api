b1abf1f07193b9c46d98014ef80bc5a5ab32d184
chucklapress <charleslapress@gmail.com>
2016-06-27 15:13:11 -0400

    added remaining classes

--- main.py
+++ main.py
@@ -8,4 +8,39 @@ while response['next']:
     url = response["next"]
     response = requests.get(url).json()
 
-    
+url = "http://swapi.co/api/planets/"
+response = requests.get(url).json()
+while response['next']:
+    for planet in response['results']:
+        print(planet['name'])
+    url = response["next"]
+    response = requests.get(url).json()
+
+url = "http://swapi.co/api/films"
+response = requests.get(url).json()
+for film in response['results']:
+    print(film['title'])
+
+url = "http://swapi.co/api/vehicles/"
+response = requests.get(url).json()
+while response['next']:
+    for vehicle in response['results']:
+        print(vehicle['name'])
+    url = response["next"]
+    response = requests.get(url).json()
+
+url = "http://swapi.co/api/starships/"
+response = requests.get(url).json()
+while response['next']:
+    for starship in response['results']:
+        print(starship['name'])
+    url = response["next"]
+    response = requests.get(url).json()
+
+url = "http://swapi.co/api/species/"
+response = requests.get(url).json()
+while response['next']:
+    for species in response['results']:
+        print(species['name'])
+    url = response["next"]
+    response = requests.get(url).json()
