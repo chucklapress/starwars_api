fc68f052df8a6cc935b4a5f4aa03a180a42d596b
chucklapress <charleslapress@gmail.com>
2016-06-27 17:49:34 -0400

    refactored all the list into a useable functioning base

--- main.py
+++ main.py
@@ -1,46 +1,46 @@
 import requests
 
-url = "http://swapi.co/api/people/"
-response = requests.get(url).json()
-while response['next']:
-    for people in response['results']:
-        print(people['name'])
-    url = response["next"]
+choice = int(input('Type (1) for a list of StarWars Universe characters, or: type (2)for a list of starshipsClasses from the movies'))
+if choice == 1:
+    url = "http://swapi.co/api/people/"
     response = requests.get(url).json()
-
-url = "http://swapi.co/api/planets/"
-response = requests.get(url).json()
-while response['next']:
-    for planet in response['results']:
-        print(planet['name'])
-    url = response["next"]
+    while response['next']:
+        for people in response['results']:
+            print(people['name'])
+        url = response["next"]
+        response = requests.get(url).json()
+elif choice == 2:
+    url = "http://swapi.co/api/starships/"
     response = requests.get(url).json()
+    while response['next']:
+        for starship in response['results']:
+            print(starship['name'])
+        url = response["next"]
+        response = requests.get(url).json()
 
-url = "http://swapi.co/api/films"
-response = requests.get(url).json()
-for film in response['results']:
-    print(film['title'])
-
-url = "http://swapi.co/api/vehicles/"
-response = requests.get(url).json()
-while response['next']:
-    for vehicle in response['results']:
-        print(vehicle['name'])
-    url = response["next"]
+choice = int(input('Type (1) for a list of species droid, humanoid other, or; type (2) to list the vehicles from the movies' ))
+if choice == 1:
+    url = "http://swapi.co/api/species/"
     response = requests.get(url).json()
-
-url = "http://swapi.co/api/starships/"
-response = requests.get(url).json()
-while response['next']:
-    for starship in response['results']:
-        print(starship['name'])
-    url = response["next"]
+    while response['next']:
+        for species in response['results']:
+            print(species['name'])
+            url = response["next"]
+            response = requests.get(url).json()
+elif choice == 2:
+    url = "http://swapi.co/api/vehicles/"
     response = requests.get(url).json()
+    while response['next']:
+        for vehicle in response['results']:
+            print(vehicle['name'])
+        url = response["next"]
+        response = requests.get(url).json()
 
-url = "http://swapi.co/api/species/"
-response = requests.get(url).json()
-while response['next']:
-    for species in response['results']:
-        print(species['name'])
-    url = response["next"]
+choice = int(input('Type(1)for a list of titles of the StarWars movies as they appeared in chronological order or type (2) to exit'))
+if choice == 1:
+    url = "http://swapi.co/api/films"
     response = requests.get(url).json()
+    for film in response['results']:
+        print(film['title'])
+elif choice ==2:
+    exit()
